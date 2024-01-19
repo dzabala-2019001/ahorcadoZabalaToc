857893ab370fe499c6670abd501a2a70ab38192c
Informatica <informatica@alumno.local>
2024-01-19 09:08:52 -0600

    segundo push

---
+++ jugabilidad.js
@@ -0,0 +1,8 @@
+let palabrita;
+let cant_errores = 0; 
+let cant_aciertos = 0; 
+const dibujar = document.getElementById("canvas");
+
+const palabras = ['palabra','computadora', 'mouse','teclado'];
+
+
